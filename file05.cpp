f0423ddc8f47b71e57a9ec3962f2c24ba9de9d10
Ahmed LAHMIDI <devlahmidi@gmail.com>
2025-11-11 16:40:48 +0100

    moified user message

---
+++ file05.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<cmath>
+#include<string> 
+
+using namespace std;
+
+int readnumbers(string Message)
+{
+	int number = 0;
+
+	do
+	{
+		cout << Message << endl;
+		cin >> number;
+	} while (number <= 0);
+
+	return number;
+	
+}
+int normalToRevers( int number)
+{
+	int remainder = 0, revers = 0;
+
+	while (number > 0)
+	{
+		remainder = number % 10;
+		number = number / 10;
+
+		revers = revers * 10 + remainder;
+		
+	}
+	return revers;
+
+}
+void printreverseToNormal(int number)
+{
+	
+	int remainder = 0;
+	
+	while (number > 0)
+	{
+		remainder = number  % 10;
+		number = number / 10;
+
+		cout << remainder << endl;
+		
+	}
+
+}
+
+int main()
+{
+	
+	int number = readnumbers("enter a number");
+	
+	printreverseToNormal(normalToRevers(number));
+	
+		
+	return 0;
+}
+
