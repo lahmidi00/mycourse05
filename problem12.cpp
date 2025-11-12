e8110597075a935f21c9d7d419ec0bfdbfa0bf9c
Ahmed LAHMIDI <devlahmidi@gmail.com>
2025-11-12 17:12:08 +0100

    solving the problem number 12

---
+++ problem12.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cmath>
+#include<string> 
+
+using namespace std;
+
+int readnumbers(string Message)
+{
+	int number = 0;
+
+	do
+	{
+		cout << Message << endl;
+		cin >> number;
+	} while (number <= 0);
+
+	return number;
+
+}
+void InvertedNumberPattern(int number)
+{
+
+	for (int i = number; i >= 1; i--)
+	{
+	
+		for (int j = 1; j <= i; j++)
+		{
+			cout << i;
+			
+		}
+		cout << endl;
+	
+	}
+}
+int main()
+{
+	
+	 InvertedNumberPattern(readnumbers("enter a positive number"));
+	
+		
+	return 0;
+}
